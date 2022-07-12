f9f3043129a757b4a5c15b4e8a8b65ac280ac7f6
PaulT <ptimofejev@gmail.com>
2022-07-12 14:13:03 +0300

    after two lessons

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
+import { FC } from 'react';
+import { Link, Route, Routes, Outlet, NavLink } from 'react-router-dom';
 import './App.scss';
+
+const TodosNavigation: FC = () => (
+  <div>
+    <Link to="all">All</Link>
+    {'  '}
+    <Link to="active">Active</Link>
+    {'  '}
+    <Link to="completed">Completed</Link>
+
+    <Outlet />
+  </div>
+);
+
+const ErrorPage: FC = () => {
+  return (<p>Page not found</p>)
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <nav>
+        <NavLink
+          to="/"
+          className={({ isActive }) => isActive ? 'isActive' : ''}
         >
-          Learn React
-        </a>
-      </header>
+          Home
+        </NavLink>
+        {'  '}
+        <NavLink
+          to="todos"
+          className={({ isActive }) => isActive ? 'isActive' : ''}
+          style={({ isActive }) => ({
+            color: isActive ? 'red' : 'green',
+          })}
+        >
+          TODOs page
+        </NavLink>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<h1>Home page</h1>}/>
+
+        <Route path="/todos" element={<TodosNavigation />}>
+          <Route index element={<h1>TODOs home page</h1>}/>
+          <Route path="all" element={<p>All TODOs</p>}/>
+          <Route path="active" element={<p>Active TODOs</p>}/>
+          <Route path="completed" element={<p>Completed TODOs</p>}/>
+        </Route>
+
+        <Route path="*" element={<ErrorPage />}/>
+      </Routes>
     </div>
   );
 }
